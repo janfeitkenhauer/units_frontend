55f72a3e03f06fa453c39501baa9e0a89b8470b0
Jan Feitkenhauer <jf85@tu-clausthal.de>
2024-09-26 22:21:51 +0200

    layout - search bar on page

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Header />
+        <Search />
         {children}
         {/* <Footer /> */}
       </body>
@@ -16,9 +17,8 @@ export default function RootLayout({ children }) {
 
 function Header() {
   return (
-    <header className='sticky top-0 w-full flex flex-col px-6 backdrop-blur-md shadow-sm flex items-center justify-center'>
-      <h1 className='text-xl font-semibold mt-4'>Semantic Unit Library</h1>
-      <Search />
+    <header className='sticky top-0 w-full h-16 flex flex-col px-6 backdrop-blur-md shadow-sm flex items-center justify-center'>
+      <h1 className='text-xl font-semibold'>Semantic Unit Library</h1>
     </header>
   );
 }
